2cc2a5c2a05d13b937ed2d63666f9a91d1beb56b
Comisión de regulación de comunicaciones <95722715+postdatacrc@users.noreply.github.com>
2022-06-25 08:53:45 -0500

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,5 +1,19 @@
 import numpy as np
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+import streamlit as st
+from plotly.subplots import make_subplots
+import glob
+import os
+from urllib.request import urlopen
+import json
+from streamlit_folium import folium_static
+from st_aggrid import AgGrid
+import geopandas as gpd
+import folium
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
